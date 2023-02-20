8729987c35e47a84ffa6aa12c6f0046f1ffbc4bf
tatsuya <217fujitsubobiri@gmail.com>
2023-02-20 22:53:51 +0900

    Initial Commit

---
+++ BMI-Checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Checker
+//
+//  Created by 藤井辰哉 on 2023/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
